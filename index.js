1f0cbeb47ec9fcd1f29e68920ceb37731671abd0
eunji kang <miki2323@naver.com>
2025-03-27 19:18:30 +0900

    feature: ShowTodayUpdatedPost

--- index.js
+++ index.js
@@ -21,9 +21,7 @@ class OpenAIService {
                 role: 'system',
                 content: `너는 디스코드 내에 존재하는 챗봇이고,
                 너의 이름은 사과방 핑거 프로텍터야.
-                디스코드의 특정 채널에 오고간 대화들을 학습하여
-                사용자들에게 친절하게 대답해주는 역할을 해줄거야.
-                다정하고 깜찍하고 활기찬 말투의 소유자야.`
+                너는 다정하고 깜찍하고 활기찬 말투의 소유자야.`
               }
         ];
     }
@@ -55,7 +53,6 @@ class DiscordBot {
             console.log(`Logged in as ${this.client.user.tag}`);
         });
 
-        // message create event 등록
         this.client.on('messageCreate', (message) => this.handleMessage(message));
     }
 
@@ -69,29 +66,47 @@ class DiscordBot {
       if (message.content.startsWith('!ask')) 
       {
           await this.handleAskMessage(message);
-      } else if (message.content.startsWith('!sum')) 
+      } 
+      else if (message.content.startsWith('!sum')) 
       {
           await this.handleSumMessage(message);
-      } else if (message.content.startsWith('!forumPosts')) 
+      } 
+      else if (message.content.startsWith('!forumPosts')) 
       {
+        await this.GetForumPosts(message);
+      }
+      else if (message.content.startsWith('!today')) 
+      {
+        await this.ShowTodayPosts(message);
+      }
+  }
+
+    async GetForumPosts(message) {
         const author = '은지캉';
-        const posts = await this.getForumPosts(author, FORUM_CHANNEL_ID); // 포럼 게시글 가져오기
+        const posts = await this.getTodayPosts(FORUM_CHANNEL_ID); // 포럼 게시글 가져오기
 
         const responses = await Promise.all(posts.map(async (post) => {
             const openAIprompt = this.openAIService.buildInitialOpenAIMessages();
 
-            openAIprompt.push({ 
-                role: 'user', 
-                content: `다음 내용을 최대 10줄로 요약해줘. ${post.content}` 
+            openAIprompt.push({
+                role: 'user',
+                content: `다음 내용을 최대 10줄로 요약해줘. ${post.content}`
             });
-        
+
             const summerizedResponse = await this.openAIService.getResponse(openAIprompt);
             return `${post.author}님의 아임파인더입니다! - ${post.link}\n: ${summerizedResponse}`;
         }));
-        
+
         message.reply(`[아임파인더]\n${responses.join('\n')}`);
-      }
-  }
+    }
+
+    async ShowTodayPosts(message) {
+        const posts = await this.getTodayPosts(FORUM_CHANNEL_ID); // 포럼 게시글 가져오기
+        const response = posts.map(post => {
+            return `${post.link} - ${post.author}\n`;
+        })
+        message.reply(`[오늘의 게시글]\n${response}`);
+    }
 
     async handleAskMessage(message) {
         const userMessageContent = this.extractUserMessage(message.content);
@@ -114,17 +129,32 @@ class DiscordBot {
         openAIMessages.push({ role: 'assistant', content: response });
 
         this.replyToMessage(message, response);
+    }   
+    async getTodayPosts(forumChannelId) {
+        const today = new Date();
+        const isToday = (thread) => {
+            const threadCreatedAt = new Date(thread.createdAt);
+            return isSameDate(threadCreatedAt, today);
+        };
+        
+        return await this.fetchThreads(forumChannelId, isToday);
+    }
+    
+    async getIamFinder(forumChannelId) {
+        return await this.fetchThreads(forumChannelId);
     }
 
-    async getForumPosts(author, forumChannelId) {
+    async fetchThreads(forumChannelId, filterFunc = null) {
         const channel = await this.client.channels.fetch(forumChannelId);
-        const threads = await channel.threads.fetchActive(); // 활성화된 스레드 가져오기 (스레드 == 게시글)
+        const threads = await channel.threads.fetchActive(); // 활성화된 스레드 가져오기
         const threadData = [];
     
         for (const thread of threads.threads.values()) { // for...of 루프 사용
             const starterMessage = await thread.fetchStarterMessage(); // 스레드의 시작 메시지를 가져오기
-    
-            // 단일 메시지인 starterMessage 사용
+            
+            if (filterFunc && !filterFunc(thread)) {
+                continue; // 필터 함수가 주어지고, 조건을 만족하지 않으면 스킵
+            }
             threadData.push({
                 id: thread.id,
                 title: thread.name,
@@ -145,6 +175,12 @@ class DiscordBot {
         message.reply(response);
     }
 }
+
+const isSameDate = (date1, date2) => { 
+    return date1.getFullYear() === date2.getFullYear() 
+    && date1.getMonth() === date2.getMonth() 
+    && date1.getDate() === date2.getDate(); 
+  } 
 
 // 봇 인스턴스 생성 및 시작
 const bot = new DiscordBot();
