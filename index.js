3d6b4c7154d314f78e21e7513d6c798a1316a87d
eunji kang <miki2323@naver.com>
2025-04-01 20:49:41 +0900

    Port

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 require('dotenv').config();
+
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+app.listen(PORT, () => {
+    console.log(`서버가 포트 ${PORT}에서 실행 중입니다.`);
+});
+
 const { Partials,  Client, GatewayIntentBits} = require('discord.js');
 const OpenAI = require('openai');
 
@@ -175,7 +189,7 @@ class DiscordBot {
     }
 
     async handleAskMessage(message) {
-        const userMessageContent = this.extractUserMessage(message.content);
+        const userMessageContent = this.extractUserMessage(messaggite.content);
         this.userMessages.push({ role: 'user', content: userMessageContent });
 
         const response = await this.openAIService.getResponse(this.userMessages);
